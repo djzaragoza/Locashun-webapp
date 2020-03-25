ed402fe07a295ccd37d05d72a853fece5958c707
unknown <dj.zaragoza@icloud.com>
2020-03-25 13:02:41 -0700

    finished coding out the js file for app functionality

--- index.js
+++ index.js
@@ -27,4 +27,45 @@ function update(weather) {
     icon.src = "https://openweathermap.org/img/w/" + weather.icon + ".png";
 }
 
+window.onload = function () {
+    icon = document.getElementById("icon");
+    temp = document.getElementById("temp");
+    loc = document.getElementById("loc");
 
+    if (this.navigator.geolocation) {
+        var showPosition = function (position) {
+            updateByGeo(position.coords.latitude.position.coords.longitude);
+        }
+        this.navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+
+function updateByGeo(lat, lon) {
+    var url = "https://api.openweathermap.org/data/2.5/weather?" + "lat=" + lat + "&lon=" + lon + "&APPID=" + APPID;
+    sendRequest(url);
+}
+
+function K2F(k) {
+    return Math.floor(9 / 5 * (k - 273) + 32);
+}
+
+function cToF(celsius) {
+    return Math.floor(celsius * 9 / 5 + 32);
+}
+
+function fToC(fahrenheit) {
+    return Math.floor((fahrenheit - 32) * 5 / 9);
+}
+
+function toggleScale() {
+    if (temScale.innerHTML === "C") {
+        temp.innerHTML = cToF(temp.innerHTML);
+        temScale.innerHTML = "F";
+    } else if (temScale.innerHTML === "F") {
+        temp.innerHTML = fToC(temp.innerHTML);
+        temScale.innerHTML = "C";
+    }
+}
+
+// toggle the temperature scale
+temScale.addEventListener("click", toggleScale);
